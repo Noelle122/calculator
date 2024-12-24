113342a3fe8672fc1efb719ea11354198106fcfa
Noelle122 <noellehartman@icloud.com>
2024-12-23 20:35:13 -0500

    Rough draft of event listeners implemented

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function displayResult(display, result){
 function opCheck(expression){ //returns true if last char is operator
     if(expression.length<=1)
         return false;
-    let lastChar=expression[expression.length-1];
     if(lastChar=='+' || lastChar=='-' || lastChar=='/' || lastChar=='*')
         return true;
     return false;
@@ -37,6 +36,7 @@ function opCheck(expression){ //returns true if last char is operator
 
 let num1, num2, operator, clickCount=0; //handles numerical input;
 let expression=[];
+//expression.length=3;
 let result;
 
 let buttons=document.querySelector(".numButtons");
@@ -47,110 +47,110 @@ buttons.addEventListener("click", (event) => {
     if (target.classList.contains('btn0')){
         if(opCheck(expression)){
             num2+='0';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='0';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn1')){
         if(opCheck(expression)){
             num2+='1';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='1';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn2')){
         if(opCheck(expression)){
             num2+='2';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='2';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn3')){
         if(opCheck(expression)){
             num2+='3';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='3';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn4')){
         if(opCheck(expression)){
             num2+='4';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='4';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn5')){
         if(opCheck(expression)){
             num2+='5';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='5';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn6')){
         if(opCheck(expression)){
             num2+='6';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='6';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn7')){
         if(opCheck(expression)){
             num2+='7';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='7';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn8')){
         if(opCheck(expression)){
             num2+='8';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='8';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
     else if (target.classList.contains('btn9')){
         if(opCheck(expression)){
             num2+='9';
-            expression.push(num2)
+            expression[2]=num2;
         }
         else{
             num1+='9';
-            expression.push(num1);
+            expression[0]=num1;
         }
         display(display, expression);
     }
@@ -160,15 +160,51 @@ let opbuttons=document.querySelector(".opButtons");
 buttons.addEventListener("click", (event) => {
     let target=event.target;
     if(target.classList.contains('btnP')){
-
+        if(!opCheck(expression) && expression.length==1){
+            expression.push('+');
+            expression.length++;
+            display(display, expression);
+        }
     }
     else if(target.classList.contains('btnS')){
-
+        if(!opCheck(expression) && expression.length==1){
+            expression.push('-');
+            expression.length++;
+            display(display, expression);
+        }
     }
     else if(target.classList.contains('btnM')){
-
+        if(!opCheck(expression) && expression.length==1){
+            expression.push('*');
+            expression.length++;
+            display(display, expression);
+        }
     }
     else if(target.classList.contains('btnD')){
-
+        if(!opCheck(expression) && expression.length==1){
+            expression.push('/');
+            expression.length++;
+            display(display, expression);
+        }
+    }
+    else if(target.classList.contains('btnE')){
+        if(expression.length==3){
+            num1=parseInt(expression[0]);
+            operator=expression[1];
+            num2=parseInt(expression[2]);
+            result=operate(operator, num1, num2);
+            result=String(result);
+            displayResult(display, result);
+            num1=result;
+            expression.length=0;
+            expression.push(num1);
+        }
+    }
+    else if(target.classList.contains('btnClear')){
+        expression.length=0;
+        result='';
+        num1='';
+        num2='';
+        display(display, expression);
     }
 });
